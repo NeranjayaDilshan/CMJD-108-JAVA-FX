a85b73f9adb68c599df34a3c1ef75585b48fe8a8
NeranjayaDilshan <neranjayadilshan@gmail.com>
2024-12-21 16:56:47 +0530

    getText() method

--- src/App.java
+++ src/App.java
@@ -14,10 +14,10 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        URL resource = getClass().getResource("./view/Main.fxml");
+        URL resource = getClass().getResource("./view/Customer.fxml");
         Parent root = FXMLLoader.load(resource);
         primaryStage.setScene(new Scene(root));
-        primaryStage.setTitle("This is first fxml");
+        primaryStage.setTitle("Customer Form");
         primaryStage.show();
     }
 
--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -1,5 +1,42 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class CustomerController {
 
+    @FXML
+    private TextArea txtAddress;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtTitle;
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+        System.out.println("Customer Delete Button is working fine");
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+        String id = txtId.getText();
+        String name = txtName.getText();
+        String address = txtAddress.getText();
+        String title = txtTitle.getText();
+
+        System.out.println("id : "+id);
+        System.out.println("name : "+name);
+        System.out.println("address : "+address);
+        System.out.println("title : "+title); 
+
+        System.out.println("Customer Save Button is working fine");
+    }
 }
