6eaed6dee78f9209cdd37808ea436113874f85f7
NeranjayaDilshan <neranjayadilshan@gmail.com>
2025-01-11 16:19:21 +0530

    load new view to same view

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,13 +1,24 @@
 package controller;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
 
 public class MainController {
 
+     @FXML
+    private AnchorPane root;
+
     @FXML
-    void btnCustomerOnAction(ActionEvent event) {
+    void btnCustomerOnAction(ActionEvent event) throws IOException {
         System.out.println("Customer Button clicked");
+        root.getChildren().clear();
+        Parent node = FXMLLoader.load(getClass().getResource("../view/Customer.fxml"));
+        root.getChildren().add(node);
     }
 
     @FXML
